b0b928d3ea78c4f170c0df368117dce810ac961e
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-06-01 18:17:09 +0000

    Testing BAM input functionality

--- run.MouseTrap2.docker.sh
+++ run.MouseTrap2.docker.sh
@@ -12,6 +12,7 @@
 
 FQ1="/data2/NIX5.10K.R1.fastq.gz"
 FQ2="/data2/NIX5.10K.R2.fastq.gz"
+BAM="/data2/human.sort.bam"
 
 HGFA="/data1/GRCh37-lite.fa"
 MMFA="/data1/Mus_musculus.GRCm38.dna_sm.primary_assembly.fa"
@@ -19,4 +20,5 @@ MMFA="/data1/Mus_musculus.GRCm38.dna_sm.primary_assembly.fa"
 OUTDIR="./results"
 mkdir -p $OUTDIR
 
-bash MouseTrap2.sh -1 $FQ1 -2 $FQ2 -h $HGFA -m $MMFA -o $OUTDIR
+#bash MouseTrap2.sh -1 $FQ1 -2 $FQ2 -h $HGFA -m $MMFA -o $OUTDIR
+bash MouseTrap2.sh -b $BAM -h $HGFA -m $MMFA -o $OUTDIR
