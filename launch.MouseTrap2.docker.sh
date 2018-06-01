e6334f4e29d825a8fa055a52c4e48346d62cb7a3
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-06-01 13:14:32 -0500

    Testing BAM processing

--- launch.MouseTrap2.docker.sh
+++ launch.MouseTrap2.docker.sh
@@ -16,13 +16,15 @@ DATA2="/Users/mwyczalk/Projects/Rabix/MouseTrap2/test-dat"
 
 FQ1="/data2/NIX5.10K.R1.fastq.gz"
 FQ2="/data2/NIX5.10K.R2.fastq.gz"
+BAM="/data2/human.sort.bam"
 
 HGFA="/data1/GRCh37-lite.fa"
 MMFA="/data1/Mus_musculus.GRCm38.dna_sm.primary_assembly.fa"
 
 OUTDIR="./results"
 
-CMD="/bin/bash MouseTrap2.sh -1 $FQ1 -2 $FQ2 -h $HGFA -m $MMFA -o $OUTDIR"
+#CMD="/bin/bash MouseTrap2.sh -1 $FQ1 -2 $FQ2 -h $HGFA -m $MMFA -o $OUTDIR"
+CMD="/bin/bash MouseTrap2.sh -b $BAM -h $HGFA -m $MMFA -o $OUTDIR"
 
 #MEMGB=8
 #LSF_ARGS="-R \"rusage[mem=${MEMGB}000]\" -M ${MEMGB}000000"
