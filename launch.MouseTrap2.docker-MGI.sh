95c44a5d83b8705a451f5c5982d4efc8f6bc1cda
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2018-06-01 12:02:20 -0500

    Initial commit of MGI-based docker launcher

---
+++ launch.MouseTrap2.docker-MGI.sh
@@ -0,0 +1,29 @@
+# Launch docker container and run MouseTrap2.  Specific to MGI
+
+# Arguments
+# -b BAM: Pass BAM to be processed
+# -1 FQ1 -2 FQ2: Pass two FASTQ files with reads1, reads2, respectively.
+# -h FASTA: human reference.  Required
+# -m FASTA: mouse reference.  Required
+# -s sample: sample name.  Default is "hgmm"
+# -o outdir: output directory.  Default is '.'
+# -d: dry run.  Print commands but do not execute them
+
+IMAGE_MGI="cgc-images.sbgenomics.com/m_wyczalkowski/disambiguate"
+
+FQ1="/gscuser/mwyczalk/projects/Rabix/MouseTrap2/test-dat/NIX5.10K.R1.fastq.gz"
+FQ2="/gscuser/mwyczalk/projects/Rabix/MouseTrap2/test-dat/NIX5.10K.R2.fastq.gz"
+
+HGFA="/gscmnt/gc2521/dinglab/mwyczalk/somatic-wrapper-data/image.data/A_Reference/GRCh37-lite.fa"
+MMFA="/gscmnt/gc2737/ding/hsun/data/ensemble_v91/Mus_musculus.GRCm38.dna_sm.primary_assembly.fa"
+
+OUTDIR="./results"
+mkdir -p $OUTDIR
+
+CMD="/bin/bash MouseTrap2.sh -1 $FQ1 -2 $FQ2 -h $HGFA -m $MMFA -o $OUTDIR"
+
+#MEMGB=8
+#LSF_ARGS="-R \"rusage[mem=${MEMGB}000]\" -M ${MEMGB}000000"
+
+bsub -q research-hpc $LSF_ARGS -Is -a "docker($IMAGE_MGI)" $CMD
+
