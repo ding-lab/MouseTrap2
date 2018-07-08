e99a0f4fe76eae8bd49baddfc92d281bb2a24ec0
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-07-08 02:13:58 -0500

    Adding demo of running MouseTrap2 from command line

---
+++ testing/README.md
@@ -0,0 +1,1 @@
+* run.MouseTrap2.CWL-workflow.sh is meant to run the whole shebang from the command line of host machine
--- testing/run.MouseTrap2.CWL-workflow.sh
+++ testing/run.MouseTrap2.CWL-workflow.sh
@@ -1,15 +1,25 @@
-RABIX="rabix"
-#CWL="../cwl/mousetrap2.cwl"
-CWL="../cwl/RemoveMouseReads.workflow.cwl"  # testing workflow
+# Script to demonstrate calling MouseTrap2 workflow from command line
+
+# Data not provided with distribution.  Here, use the same FASTQ data for human and PDX
+# TODO: show how test dataset created
+
+SAMPLE="NIX5-test"
 
 # try to have all output go to output_dir
 OUTD="results"
 mkdir -p $OUTD
 RABIX_ARGS="--basedir $OUTD"
 
-# reference stuff in /data1
+
+# This is a test database associated with TinDaisy demo dataset
+DEMOD="/Users/mwyczalk/Projects/Rabix/TinDaisy/StrelkaDemo.dat"
+DBSNP_DB="$DEMOD/dbsnp-StrelkaDemo.noCOSMIC.vcf.gz"
+PINDEL_CONFIG="$DEMOD/pindel.WES.ini"
+VARSCAN_CONFIG="$DEMOD/varscan.WES.ini"
+STRELKA_CONFIG="$DEMOD/strelka.WES.ini"
+
+# reference stuff in /data1, test FASTQs in /data2
 DATA1="/Users/mwyczalk/Data/SomaticWrapper/image/A_Reference"
-# test FASTQs in /data2
 DATA2="/Users/mwyczalk/Projects/Rabix/MouseTrap2/test-dat"
 
 FQ1="$DATA2/NIX5.10K.R1.fastq.gz"
@@ -17,7 +27,41 @@ FQ2="$DATA2/NIX5.10K.R2.fastq.gz"
 
 HGFA="$DATA1/GRCh37-lite.fa"
 MMFA="$DATA1/Mus_musculus.GRCm38.dna_sm.primary_assembly.fa"
+ASSEMBLY="GRCh37"
 
-SAMPLE="NIX5-test"
+ARGS="\
+--assembly $ASSEMBLY \
+--dbsnp_db $DBSNP_DB \
+--FQ1 $FQ1 \
+--FQ2 $FQ2 \
+--FQ1_PDX $FQ1 \
+--FQ2_PDX $FQ2 \
+--HGFA $HGFA \
+--MMFA $MMFA \
+--pindel_config $PINDEL_CONFIG \
+--SampleName $SAMPLE \
+--strelka_config $STRELKA_CONFIG \
+--varscan_config $VARSCAN_CONFIG"
 
-$RABIX $RABIX_ARGS $CWL -- --FQ1 $FQ1 --FQ2 $FQ2 --HGFA $HGFA --MMFA $MMFA --SAMPLE $SAMPLE
+RABIX="rabix"
+CWL="../cwl/mousefilter2-tindaisy-workflow.cwl"
+#    --assembly <arg> - GRCh37 or GRCh38
+#    --centromere_bed <arg> - optional.  Used for pindel
+#    --dbsnp_db <arg> - database used for DBSNP filter
+#    --FQ1 <arg> - reads 1, in FASTQ format
+#    --FQ1_PDX <arg>
+#    --FQ2 <arg> - reads 2, in FASTQ format
+#    --FQ2_PDX <arg>
+#    --HGFA <arg> - Human reference
+#    --is_strelka2 - boolean, optional.  Set to use Strelka2 as variant caller instead of Strelka v1
+#    --MMFA <arg> - Mouse reference
+#    --no_delete_temp <arg> - if set to 1, keep all intermediate files
+#    --output_vep <arg> - if 1, output in VEP instead of VCF format
+#    --pindel_config <arg> - PINDEL configuration file
+#    --SampleName <arg> - Run identifier
+#    --strelka_config <arg> - Strelka configuration file
+#    --varscan_config <arg> - Varscan configuration file
+#    --vep_cache_gz <arg> - optional path to compressed VEP cache file.  If defined, will not perform online VEP queries
+#       vep_cache_gz should be used for significantly improved performance in VEP annotation
+
+$RABIX $RABIX_ARGS $CWL -- $ARGS
