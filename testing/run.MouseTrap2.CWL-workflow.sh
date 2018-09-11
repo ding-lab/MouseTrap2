40c9978e67857b90333d1251c0178d769c91d384
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-09-10 19:37:21 -0500

    Successfully tested on Denali
    * Tagging version as 20180910 to correspond to similarly tagged TinDaisy and SomaticWrapper versions
    * Ran with cgc-images.sbgenomics.com/m_wyczalkowski/somatic-wrapper:20180910

--- testing/run.MouseTrap2.CWL-workflow.sh
+++ testing/run.MouseTrap2.CWL-workflow.sh
@@ -3,6 +3,8 @@
 # Data not provided with distribution.  Here, use the same FASTQ data for human and PDX
 # TODO: show how test dataset created
 
+# Certain parameters from /diskmnt/Projects/Users/hsun/beta_tinDaisy/tin-daisy/test.C3N-01649/project_config.C3N-01649-test.sh
+
 SAMPLE="NIX5-test"
 
 # try to have all output go to output_dir
@@ -13,13 +15,15 @@ RABIX_ARGS="--basedir $OUTD"
 
 # This is a test database associated with TinDaisy demo dataset
 DEMOD="../params"
-DBSNP_DB="test-data/dbsnp-StrelkaDemo.noCOSMIC.vcf.gz"
+#DBSNP_DB="test-data/dbsnp-StrelkaDemo.noCOSMIC.vcf.gz"
+DBSNP_DB="/diskmnt/Projects/Users/hsun/data/dbsnp/00-All.brief.pass.cosmic.vcf.gz"  # From Hua 
 PINDEL_CONFIG="$DEMOD/pindel.WES.ini"
 VARSCAN_CONFIG="$DEMOD/varscan.WES.ini"
 STRELKA_CONFIG="$DEMOD/strelka.WES.ini"
 
 # reference stuff in /data1, test FASTQs in /data2
-DATA1="/Users/mwyczalk/Data/SomaticWrapper/image/A_Reference"
+#DATA1="/Users/mwyczalk/Data/SomaticWrapper/image/A_Reference"
+DATA1="/diskmnt/Projects/Users/mwyczalk/data/docker/data/A_Reference"
 DATA2="./test-data"
 
 FQ1="$DATA2/NIX5.10K.R1.fastq.gz"
@@ -27,10 +31,19 @@ FQ2="$DATA2/NIX5.10K.R2.fastq.gz"
 
 HGFA="$DATA1/GRCh37-lite.fa"
 MMFA="$DATA1/Mus_musculus.GRCm38.dna_sm.primary_assembly.fa"
+
+VEP_CACHE_GZ="/diskmnt/Projects/Users/mwyczalk/data/docker/data/D_VEP/vep-cache.90_GRCh37.tar.gz"
+VEP_CACHE_VERSION="90"
 ASSEMBLY="GRCh37"
 
-ARGS="\
+# This is optional; remove this for online VEP db lookups
+CACHE="\
+--vep_cache_gz $VEP_CACHE_GZ \
+--vep_cache_version $VEP_CACHE_VERSION \
 --assembly $ASSEMBLY \
+"
+
+ARGS="\
 --dbsnp_db $DBSNP_DB \
 --FQ1 $FQ1 \
 --FQ2 $FQ2 \
@@ -47,6 +60,7 @@ ARGS="\
 --pindel_vcf_filter_config $DEMOD/pindel-vcf_filter_config.ini \
 --af_filter_config $DEMOD/af_filter_config.ini \
 --classification_filter_config $DEMOD/classification_filter_config.ini \
+$CACHE \
 "
 
 RABIX="rabix"
